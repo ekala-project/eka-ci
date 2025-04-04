0472aa7b15c2348517dc1593fef9d8348192359a
Jonathan Ringer <jonringer117@gmail.com>
2025-04-03 23:56:45 -0700

    Example of having a shell command to bring elm up or down

--- flake.nix
+++ flake.nix
@@ -21,7 +21,30 @@
           rustfmt
           elmPackages.elm
           elmPackages.elm-format
+
+          watchexec
+          live-server
         ];
+
+        shellHook = ''
+          elm_up() {
+            repo_root=$(git rev-parse --show-toplevel)
+            pushd "$repo_root/frontend"
+
+            watchexec --shell=none --watch=./src --exts=elm --log-file frontend.logs 2>&1 -- elm make ./src/Main.elm --output ./static/main.js &
+            export WATCH_PID="$!"
+
+            live-server --open ./static &
+            export LIVE_PID="$!"
+
+            popd
+          }
+
+          elm_down() {
+            [ -n WATCH_PID ] && echo "Stopping watchexec" && kill "$WATCH_PID"
+            [ -n LIVE_PID ] && echo "Stopping live-server" && kill "$LIVE_PID"
+          }
+        '';
       };
   });
 }
