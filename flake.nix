6fe8e2c281c8b63e91ea88dd5a119039b452e0d8
Jonathan Ringer <jonringer117@gmail.com>
2025-04-28 23:58:50 -0700

    [Nix] Package eka-ci

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Eka-ci flake";
+  description = "EkaCI flake";
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
@@ -11,11 +11,14 @@
       self,
       nixpkgs,
       utils,
-    }:
+    }: let
+      localOverlay = import ./nix/overlay.nix;
+    in
     utils.lib.eachDefaultSystem (system: rec {
       legacyPackages = import nixpkgs {
         inherit system;
         overlays = [
+          localOverlay
           (final: prev: {
             dev-server = final.callPackage ./nix/dev-server.nix { };
             dev-shell = final.callPackage ./nix/dev-shell.nix { };
@@ -23,7 +26,10 @@
         ];
       };
 
+      packages.default = legacyPackages.eka-ci;
       devShells.default = legacyPackages.dev-shell;
       formatter = legacyPackages.nixfmt-rfc-style;
-    });
+    }) // {
+      overlays.default = localOverlay;
+    };
 }
