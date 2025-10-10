778356a956644f2aecd0daf19c18ae4424cf817d
Jonathan Ringer <jonringer117@gmail.com>
2025-10-09 19:02:16 -0700

    devshell: add newer version of rustfmt

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -13,6 +15,7 @@
       nixpkgs,
       utils,
       treefmt-nix,
+      fenix,
     }:
     let
       localOverlay = import ./nix/overlay.nix;
@@ -21,6 +24,7 @@
       legacyPackages = import nixpkgs {
         inherit system;
         overlays = [
+          fenix.overlays.default
           localOverlay
           (final: prev: {
             dev-server = final.callPackage ./nix/dev-server.nix { };
