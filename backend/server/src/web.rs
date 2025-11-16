1d0059f51a3bdb0d21cd4befa71f3475216f9ae6
Jonathan Ringer <jonringer117@gmail.com>
2025-11-16 10:33:32 -0800

    Pass octocrab to webhandler

--- backend/server/src/web.rs
+++ backend/server/src/web.rs
@@ -2,8 +2,9 @@ use std::net::{SocketAddr, SocketAddrV4};
 
 use anyhow::{Context, Result};
 use axum::Router;
-use axum::extract::Json;
+use axum::extract::{Json, State};
 use axum::routing::{get, post};
+use octocrab::Octocrab;
 use octocrab::models::webhook_events::WebhookEventPayload as WEP;
 use tokio::net::TcpListener;
 use tokio_util::sync::CancellationToken;
@@ -11,15 +12,19 @@ use tracing::{error, info};
 
 pub struct WebService {
     listener: TcpListener,
+    octocrab: Option<Octocrab>,
 }
 
 impl WebService {
-    pub async fn bind_to_address(socket: &SocketAddrV4) -> Result<Self> {
+    pub async fn bind_to_address(
+        socket: &SocketAddrV4,
+        octocrab: Option<Octocrab>,
+    ) -> Result<Self> {
         let listener = tokio::net::TcpListener::bind(socket)
             .await
             .context(format!("failed to bind to tcp socket at {socket}"))?;
 
-        Ok(Self { listener })
+        Ok(Self { listener, octocrab })
     }
 
     pub fn bind_addr(&self) -> SocketAddr {
@@ -31,7 +36,9 @@ impl WebService {
     }
 
     pub async fn run(self, cancellation_token: CancellationToken) {
-        let app = Router::new().nest("/v1", api_routes());
+        let app = Router::new()
+            .nest("/v1", api_routes(self.octocrab.clone()))
+            .with_state(self.octocrab);
 
         if let Err(e) = axum::serve(self.listener, app)
             .with_graceful_shutdown(async move {
@@ -48,13 +55,20 @@ impl WebService {
     }
 }
 
-fn api_routes() -> Router {
-    Router::new()
-        .route("/logs/{drv}", get(get_derivation_log))
-        .route("/github/webhook", post(handle_github_webhook))
+fn api_routes(maybe_octocrab: Option<Octocrab>) -> Router<Option<Octocrab>> {
+    let mut router = Router::new().route("/logs/{drv}", get(get_derivation_log));
+
+    if maybe_octocrab.is_some() {
+        router = router.route("/github/webhook", post(handle_github_webhook));
+    }
+
+    router
 }
 
-async fn handle_github_webhook(Json(webhook_payload): Json<WEP>) {
+async fn handle_github_webhook(
+    State(_octocrab): State<Option<Octocrab>>,
+    Json(webhook_payload): Json<WEP>,
+) {
     crate::github::handle_webhook_payload(webhook_payload).await;
 }
 
